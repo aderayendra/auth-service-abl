50cb46525b3113e28109c893bdf726aefd13ff6b
Ade Rayendra <ade.rayendra@gmail.com>
2026-05-16 19:29:35 +0700

    backup

--- src/main/java/id/aderayendra/authservice/config/SecurityConfig.java
+++ src/main/java/id/aderayendra/authservice/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/api/user/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
